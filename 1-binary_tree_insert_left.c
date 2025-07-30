09ab869239394de43dcfff457ada1d20aa12dd53
sokolgj19 <gjekasokol19@gmail.com>
2025-07-30 10:33:14 +0000

    Write a function that inserts a node as the left-child of another node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_insert_left - Inserts a node as the left child of another node.
- * @parent: Pointer to the parent node.
- * @value: Value to store in the new node.
+ * binary_tree_insert_left - Inserts a node as the left-child of another node
+ * @parent: Pointer to the node to insert the left-child in
+ * @value: Value to store in the new node
  *
- * Return: Pointer to the created node, or NULL on failure or if parent is NULL.
+ * Return: Pointer to the created node, or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_insert_right - Inserts a node as the right child of another node.
- * @parent: Pointer to the parent node.
- * @value: Value to store in the new node.
+ * binary_tree_insert_right - Inserts a node as the right-child of another node
+ * @parent: Pointer to the node to insert the right-child in
+ * @value: Value to store in the new node
  *
- * Return: Pointer to the created node, or NULL on failure or if parent is NULL.
+ * Return: Pointer to the created node, or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
